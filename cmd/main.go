6e0c202647e832d0982026fd223956fb08e5fe18
Kekzploit <kekzploit@mailfence.com>
2023-03-07 01:28:45 +0000

    initial commit

---
+++ cmd/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/spf13/viper"
+)
+
+func main() {
+	// initiate environment variable config file
+	viper.SetConfigName("config")
+	viper.SetConfigType("yaml")
+	viper.AddConfigPath("./../configs/")
+	err := viper.ReadInConfig()
+	if err != nil { // Handle errors reading the config file
+		panic(fmt.Errorf("fatal error config file: %w", err))
+	}
+
+	hello := viper.Get("VALUE.HELLO").(string)
+
+	fmt.Println(hello)
+}
---
+++ pkg/tx/txcheck.go
@@ -0,0 +1,5 @@
+package tx
+
+func TxWatch() string {
+	return "Hello world"
+}
